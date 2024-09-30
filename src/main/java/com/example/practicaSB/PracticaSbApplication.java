cdb07d179bec21fc5cfdb9997348e07a7d61cc6d
Leo-Dev <leonardocaimmi1@gmail.com>
2024-09-30 18:56:24 -0300

    proyecto

---
+++ src/main/java/com/example/practicaSB/PracticaSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicaSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practicaSB/PracticaSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicaSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
