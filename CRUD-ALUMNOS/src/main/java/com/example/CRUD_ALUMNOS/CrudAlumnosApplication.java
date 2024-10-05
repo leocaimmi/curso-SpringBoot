3cff9f7ce30a34a5be0fade26d4dee43e47b372e
Leo-Dev <leonardocaimmi1@gmail.com>
2024-10-05 11:40:07 -0300

    proyecto

---
+++ CRUD-ALUMNOS/src/main/java/com/example/CRUD_ALUMNOS/CrudAlumnosApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_ALUMNOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAlumnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAlumnosApplication.class, args);
+	}
+
+}
---
+++ CRUD-ALUMNOS/src/test/java/com/example/CRUD_ALUMNOS/CrudAlumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_ALUMNOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAlumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
